5251c5a5d3e53680bcd350fa288a431abaa56fb2
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-02-13 00:47:24 +0900

    feature: add preview for loading

---
+++ src/Preview/Loading.elm
@@ -0,0 +1,50 @@
+module Preview.Loading exposing (..)
+
+import Browser
+import Html.Styled exposing (Html)
+import Loading
+import Preview.GlobalStyle
+
+
+type Msg
+    = Message
+
+
+type alias Model =
+    { statusText : String
+    }
+
+
+init : Model
+init =
+    { statusText = "Ready" }
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = view >> Html.Styled.toUnstyled
+        , update = update
+        }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Message ->
+            { model | statusText = "Loaded" }
+
+
+view : Model -> Html msg
+view model =
+    Html.Styled.div
+        []
+        [ Preview.GlobalStyle.stopAnimation
+        , Loading.view { span = 1 }
+        ]
+
+
+subscriptions : Model -> Sub msg
+subscriptions model =
+    Sub.none
