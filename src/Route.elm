adc088b320714554e7aacf38251537e980b50a2a
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-02-26 21:00:35 +0900

    feature: add route

---
+++ src/Route.elm
@@ -0,0 +1,54 @@
+module Route exposing (Route(..), history, toHistory, toString, toTop, top)
+
+
+type Transition a
+    = Transition
+
+
+type Allowed
+    = Allowed
+
+
+type Route
+    = Top
+        (Transition
+            { top : Allowed
+            , history : Allowed
+            }
+        )
+    | History
+        (Transition
+            { top : Allowed
+            , history : Allowed
+            }
+        )
+
+
+toString : Route -> String
+toString route =
+    case route of
+        Top _ ->
+            "Top"
+
+        History _ ->
+            "History"
+
+
+toTop : Transition { a | top : Allowed } -> Route
+toTop _ =
+    Top Transition
+
+
+toHistory : Transition { a | history : Allowed } -> Route
+toHistory _ =
+    History Transition
+
+
+top : Route
+top =
+    Top Transition
+
+
+history : Route
+history =
+    History Transition
