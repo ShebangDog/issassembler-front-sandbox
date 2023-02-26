6306fee29a9c758f443843e3260f42efeb2de0aa
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-02-26 21:00:48 +0900

    feature: use Browser.application

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,17 @@
 module Main exposing (Status(..), view)
 
 import Browser exposing (Document)
-import Html.Styled exposing (button, div, text)
-import Html.Styled.Events exposing (onClick)
-import Loading
-import None
+import Browser.Navigation
+import Html.Styled exposing (a, div, text)
+import Html.Styled.Attributes exposing (href)
+import Route exposing (Route)
+import Url
+import Url.Parser exposing (Parser)
 
 
 type Msg
-    = Message
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Route
 
 
 type Status
@@ -18,64 +21,90 @@ type Status
 
 type alias Model =
     { status : Status
+    , key : Browser.Navigation.Key
+    , route : Route
     }
 
 
-init : () -> ( Model, Cmd msg )
-init () =
-    ( { status = Loading }
+init : () -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
+init () url key =
+    ( Model Loading key (Maybe.withDefault Route.top (Url.Parser.parse routeParser url))
     , Cmd.none
     )
 
 
 main : Program () Model Msg
 main =
-    Browser.document
+    Browser.application
         { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
+        , onUrlRequest = LinkClicked
+        , onUrlChange = handleUrlChange
         }
+
+
+handleUrlChange : Url.Url -> Msg
+handleUrlChange url =
+    UrlChanged (Maybe.withDefault Route.top (Url.Parser.parse routeParser url))
+
+
+routeParser : Parser (Route -> Route) Route
+routeParser =
+    Url.Parser.oneOf
+        ([ Route.top, Route.history ]
+            |> List.map (\route -> Url.Parser.map route (Url.Parser.s (Route.toString route)))
+        )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Message ->
-            ( { model | status = Success }
-            , Cmd.none
-            )
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Browser.Navigation.pushUrl model.key (Url.toString url) )
 
+                Browser.External href ->
+                    ( model, Browser.Navigation.load href )
 
-statusText : Status -> String
-statusText status =
-    case status of
-        Loading ->
-            "fetch"
-
-        Success ->
-            "loaded"
+        UrlChanged route ->
+            ( { model | route = route }, Cmd.none )
 
 
 view : Model -> Document Msg
 view model =
-    { title = "Issassembler"
-    , body =
-        List.map Html.Styled.toUnstyled
-            [ div
-                []
-                [ button [ onClick Message ]
-                    [ text (statusText model.status)
+    case model.route of
+        Route.Top from ->
+            { title = "main"
+            , body =
+                List.map Html.Styled.toUnstyled
+                    [ div
+                        []
+                        [ text "main"
+                        , div [] [ a [ transition (Route.toHistory from) ] [ text "History" ] ]
+                        , div [] [ a [ transition (Route.toTop from) ] [ text "Main" ] ]
+                        ]
                     ]
-                , case model.status of
-                    Loading ->
-                        Loading.view { span = 2 }
+            }
 
-                    Success ->
-                        None.view
-                ]
-            ]
-    }
+        Route.History from ->
+            { title = "hisotry"
+            , body =
+                List.map Html.Styled.toUnstyled
+                    [ div
+                        []
+                        [ text "history"
+                        , a [ transition (Route.toTop from) ] [ text "Main" ]
+                        ]
+                    ]
+            }
+
+
+transition : Route -> Html.Styled.Attribute Msg
+transition route =
+    href ("/" ++ Route.toString route)
 
 
 subscriptions : Model -> Sub msg
