2df2aeace37806694498634b15f3ded9bdc0af12
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-03-04 23:37:24 +0900

    feature: define DisplayMode

--- src/Color.elm
+++ src/Color.elm
@@ -1,6 +1,7 @@
-module Color exposing (palette, theme)
+module Color exposing (DisplayMode(..), defaultTheme, palette, theme)
 
 import Css
+import Theme exposing (Theme)
 
 
 palette : { turkishRose : Css.Color, apache : Css.Color, flatBlue : Css.Color }
@@ -11,9 +12,48 @@ palette =
     }
 
 
-theme : { primary : Css.Color, secondary : Css.Color, tertiary : Css.Color }
-theme =
+type alias Theme =
+    { primary : Css.Color
+    , secondary : Css.Color
+    , tertiary : Css.Color
+    }
+
+
+defaultTheme : Theme
+defaultTheme =
     { primary = palette.turkishRose
     , secondary = palette.flatBlue
     , tertiary = palette.apache
     }
+
+
+lightTheme : Theme
+lightTheme =
+    defaultTheme
+
+
+darkTheme : Theme
+darkTheme =
+    { primary = palette.apache
+    , secondary = palette.flatBlue
+    , tertiary = palette.turkishRose
+    }
+
+
+type DisplayMode
+    = Default
+    | Light
+    | Dark
+
+
+theme : DisplayMode -> Theme
+theme mode =
+    case mode of
+        Default ->
+            defaultTheme
+
+        Light ->
+            lightTheme
+
+        Dark ->
+            darkTheme
