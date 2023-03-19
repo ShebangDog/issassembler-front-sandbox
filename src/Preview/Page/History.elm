596e065dbc74a94689e85fb8d585459366c34e49
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-03-19 10:50:14 +0900

    feature: register history

---
+++ src/Preview/Page/History.elm
@@ -0,0 +1,28 @@
+module Preview.Page.History exposing (..)
+
+import Browser
+import Html.Styled
+import Page
+import Preview.Page
+import Route
+
+
+init : Preview.Page.Model
+init =
+    { data = { count = 1 }
+    , route = Route.history
+    }
+
+
+view : Preview.Page.Model -> Html.Styled.Html Preview.Page.Msg
+view model =
+    Page.history model (Page.Action (\_ -> Preview.Page.Clicked))
+
+
+main : Program () Preview.Page.Model Preview.Page.Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = view >> Html.Styled.toUnstyled
+        , update = Preview.Page.update
+        }
