c1fa9323c97162af158f5a3c1ae5218a3d60ad13
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-03-19 20:15:44 +0900

    feature: add style to NavigationItem

--- src/NavigationItem.elm
+++ src/NavigationItem.elm
@@ -1,20 +1,35 @@
 module NavigationItem exposing (view)
 
-import Html.Styled exposing (a, li, p, text)
+import Css
+import Html.Styled exposing (a, img, li, p, span, text)
+import Html.Styled.Attributes exposing (css, src)
 import Route exposing (Route)
 
 
 view : (Route -> Html.Styled.Attribute msg) -> Bool -> Route -> Html.Styled.Html msg
 view transition isSelected route =
     let
+        path =
+            "../"
+                ++ (case route of
+                        Route.Top _ ->
+                            "../assets/pyramid.png"
+
+                        Route.History _ ->
+                            "../assets/black_book.png"
+                   )
+
+        icon =
+            img [ src path, css [ Css.width (Css.px 24) ] ] []
+
         content =
             text (Route.toString route)
 
         element =
             if isSelected then
-                p [] [ content ]
+                span [] [ content ]
 
             else
                 a [ transition route ] [ content ]
     in
-    li [] [ element ]
+    li [ css [ Css.padding (Css.px 8) ] ] [ icon, element ]
