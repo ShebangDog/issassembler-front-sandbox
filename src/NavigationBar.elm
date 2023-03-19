e903bdc21a4ca0552297839c7b399c6d2116fa64
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-03-19 19:10:18 +0900

    feature: make NavigationBar receive theme

--- src/NavigationBar.elm
+++ src/NavigationBar.elm
@@ -1,19 +1,30 @@
 module NavigationBar exposing (view)
 
+import Color
+import Css
 import Html.Styled exposing (div, h1, header, nav, text, ul)
+import Html.Styled.Attributes exposing (css)
 import NavigationItem
 import Route exposing (Route)
 
 
-view : String -> List Route -> Route -> (Route -> Html.Styled.Attribute msg) -> Html.Styled.Html msg
-view title routeList currentRoute transition =
+view : Color.Theme -> String -> List Route -> Route -> (Route -> Html.Styled.Attribute msg) -> Html.Styled.Html msg
+view theme title routeList currentRoute transition =
     let
         navigationItem route =
             NavigationItem.view transition (route == currentRoute) route
     in
     header
         []
-        [ h1 [] [ text title ]
+        [ h1
+            [ css
+                [ Css.backgroundColor theme.primary
+                , Css.color theme.onPrimary
+                , Css.fontSize (Css.px 32)
+                , Css.padding2 (Css.px 16) (Css.px 16)
+                ]
+            ]
+            [ text title ]
         , div []
             [ nav []
                 [ ul
