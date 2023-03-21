e9378e3ccb9a70770f34183f3a152e7570ff3a50
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-03-21 22:39:11 +0900

    feature: add preview for page

--- src/Page.elm
+++ src/Page.elm
@@ -1,9 +1,16 @@
 module Page exposing (Action, Model, history, top, view)
 
+import Browser
 import Color
-import Html.Styled exposing (button, div, text)
+import Css.Global
+import Css.Reset
+import Html.Styled exposing (Html, button, div, text)
 import Html.Styled.Events exposing (onClick)
 import Route exposing (Route(..), history)
+
+
+
+-- Implement
 
 
 type alias Model a =
@@ -54,3 +61,57 @@ view model action =
 
         Route.History _ ->
             history model action
+
+
+
+-- Preview
+
+
+type Msg
+    = None
+
+
+type alias PreviewModel =
+    Model {}
+
+
+switchRoute : Route.Route -> Route.Route
+switchRoute route =
+    case route of
+        Route.Top _ ->
+            Route.history
+
+        Route.History _ ->
+            Route.top
+
+
+update : Msg -> PreviewModel -> PreviewModel
+update msg model =
+    case msg of
+        None ->
+            { model | route = switchRoute model.route }
+
+
+preview : PreviewModel -> Html Msg
+preview model =
+    Html.Styled.div
+        [ onClick None ]
+        [ Css.Global.global Css.Reset.ericMeyer
+        , view model (Action (\_ -> None))
+        ]
+
+
+init : PreviewModel
+init =
+    { data = { count = 1 }
+    , route = Route.top
+    }
+
+
+main : Program () PreviewModel Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = preview >> Html.Styled.toUnstyled
+        , update = update
+        }
