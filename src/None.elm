4df0e14e7ef958a43799c9aaa07c169adc87eb96
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-03-21 22:34:35 +0900

    feature: add preview for None

--- src/None.elm
+++ src/None.elm
@@ -1,8 +1,41 @@
 module None exposing (view)
 
+import Browser
 import Html.Styled exposing (Html, text)
+
+
+
+-- Implement
 
 
 view : Html msg
 view =
     text ""
+
+
+
+-- Preview
+
+
+init : ()
+init =
+    ()
+
+
+main : Program () () ()
+main =
+    Browser.sandbox
+        { init = ()
+        , view = preview >> Html.Styled.toUnstyled
+        , update = update
+        }
+
+
+update : () -> () -> ()
+update _ _ =
+    ()
+
+
+preview : () -> Html msg
+preview _ =
+    view
