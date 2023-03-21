82a2f478e36981b11d6d1ad156817add9ccb705e
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-03-21 22:05:45 +0900

    feature: add preview tag to displayModeIcon

--- src/DisplayModeIcon.elm
+++ src/DisplayModeIcon.elm
@@ -1,10 +1,15 @@
 module DisplayModeIcon exposing (..)
 
+import Browser
 import Color
 import Css
 import Css.Global exposing (path)
-import Html.Styled exposing (img)
+import Html.Styled exposing (Html, img)
 import Html.Styled.Attributes exposing (css, src)
+
+
+
+-- Implement
 
 
 view : Color.DisplayMode -> Html.Styled.Html msg
@@ -31,3 +36,36 @@ view displayMode =
                 []
     in
     icon
+
+
+
+-- Preview
+
+
+init : ()
+init =
+    ()
+
+
+main : Program () () ()
+main =
+    Browser.sandbox
+        { init = init
+        , view = preview >> Html.Styled.toUnstyled
+        , update = update
+        }
+
+
+update : () -> () -> ()
+update _ _ =
+    ()
+
+
+preview : () -> Html msg
+preview _ =
+    Html.Styled.div
+        []
+        (List.map
+            view
+            Color.displayModeSet
+        )
