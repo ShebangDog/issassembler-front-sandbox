7aa526d414fa815e1eb017c258c4ec976232d7cd
ShebangDog <twilight.sky.asterisk@gmail.com>
2023-09-27 18:41:38 +0900

    upda

--- script/isExist.js
+++ script/isExist.js
@@ -1,5 +1,5 @@
-const [context, targetLabelName] = process.argv.slice(2)
+const [labels, targetLabelName] = process.argv.slice(2)
 
-const isExist = context.labels.map(label => label.name).include(targetLabelName)
+const isExist = labels.map(label => label.name).include(targetLabelName)
 
 console.log(isExist)
